e802f9027cd8d2839015c865348e36d785949067
Justin Green <justingreen.business@gmail.com>
2022-12-23 11:14:49 +1100

    Addition (SceneObject) : Attempted to make SceneObject class from c# for c++, got up to remove child function

---
+++ MathLib/SceneObject.cpp
@@ -0,0 +1,86 @@
+﻿#include "SceneObject.h"
+
+SceneObject::SceneObject()
+{
+    
+}
+
+SceneObject* SceneObject::GetParent()
+{
+    return parent;
+}
+
+MathLib::Mat3 SceneObject::GetGlobalTransform()
+{
+    return parent == nullptr ? transform : transform * parent->GetGlobalTransform();
+}
+
+template <class T>
+void SceneObject::Update()
+{
+    OnUpdate();
+
+    if (isAlive == false)
+        parent->RemoveChild(this);
+
+    for(auto action : m_updateChildActions<>)
+    {
+        action();
+    }
+    m_updateChildActions<T>.clear();
+
+    for(auto child : children)
+    {
+        child->Update<T>();
+    }
+}
+
+void SceneObject::Draw()
+{
+    OnDraw();
+    for(auto child : children)
+    {
+        child->Draw();
+    }
+}
+
+void SceneObject::OnUpdate()
+{
+}
+
+void SceneObject::OnDraw()
+{
+}
+
+void SceneObject::ItemAdd(SceneObject* _child)
+{
+    // if(_child->parent != nullptr)
+    //     _child->parent->RemoveChild(_child);
+    //
+    // _child->parent = this;
+    // children.push_back(_child);
+}
+
+void SceneObject::ItemRemove(SceneObject* _child)
+{
+    // if (_child->parent == this)
+    // {
+    //     _child->parent = nullptr;
+    //     chil
+    // }
+}
+
+template <class T>
+void SceneObject::AddChild(SceneObject* _child)
+{
+    m_updateChildActions<T>.push_back(ItemAdd(_child));
+}
+
+template <class T>
+void SceneObject::RemoveChild(SceneObject* child)
+{
+}
+
+void SceneObject::SetParent(SceneObject* parent)
+{
+}
---
+++ MathLib/SceneObject.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+#include <functional>
+#include <list>
+
+#include "Mat3.h"
+
+
+class SceneObject
+{
+public:
+    virtual ~SceneObject() = default;
+    SceneObject();
+
+    SceneObject* parent = nullptr;
+    std::list<SceneObject*> children;
+    MathLib::Mat3 transform;
+    bool isAlive = true;
+
+    SceneObject* GetParent();
+
+    MathLib::Mat3 GetGlobalTransform();
+
+    template <class T>
+    void Update();
+    void Draw();
+    virtual void OnUpdate();
+    virtual void OnDraw();
+
+    void ItemAdd(SceneObject* _child);
+    template <class T>
+    void AddChild(SceneObject* _child);
+
+    void ItemRemove(SceneObject* _child);
+    template <class T>
+    void RemoveChild(SceneObject* child);
+    void SetParent(SceneObject* parent);
+
+protected:
+    
+private:
+    
+    template <class ACTION_T>
+    std::list<ACTION_T> m_updateChildActions;
+
+    
+    
+};
