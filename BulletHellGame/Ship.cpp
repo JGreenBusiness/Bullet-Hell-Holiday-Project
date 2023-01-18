b277687ae8cc749d59a823bb3e556df8f11d8c4d
Justin Green <justingreen.business@gmail.com>
2023-01-19 09:41:44 +1100

    Refactor (Ship) : Ship movement now based on Transform multiplication and mouse direction

--- BulletHellGame/Ship.cpp
+++ BulletHellGame/Ship.cpp
@@ -33,29 +33,29 @@ void Ship::Awake()
 
 void Ship::Update()
 {
-    m_pos = Vec2(Transform.m7,Transform.m8);
+    m_pos = Transform.GetTranslation();
     m_hitBox->Position = m_pos;
-    float speed = 3.1f;
+    float speed = 5.1f;
 
-
-    if(!m_input->getPressedKeys().empty())
+    Vec2 mousePos = Vec2(m_input->getMouseX(),m_input->getMouseY());
+    Vec2 dir = Vec2::Normalise(Vec2::Subtract(mousePos,m_pos));
+    if(dir.x < 1 && dir.y < 1)
     {
-        Transform.SetTranslation(0,speed * m_input->isKeyDown(aie::INPUT_KEY_W));
-        Transform.SetTranslation(0,-speed * m_input->isKeyDown(aie::INPUT_KEY_S));
-        Transform.SetTranslation(-speed * m_input->isKeyDown(aie::INPUT_KEY_A),0);
-        Transform.SetTranslation(speed * m_input->isKeyDown(aie::INPUT_KEY_D),0);
-
-        float rotX = Transform.GetRotationX();
-        Transform.SetRotationZ(rotX += -0.1f * m_input->isKeyDown(aie::INPUT_KEY_RIGHT));
-        Transform.SetRotationZ(rotX += 0.1f * m_input->isKeyDown(aie::INPUT_KEY_LEFT));
+        Transform.SetRotationZ(std::atan2(-dir.x,dir.y));
+        
     }
-    
+
+    if(Vec2::Distance(mousePos, m_pos) > 5)
+    {
+        Transform = Mat3::Multiply(Mat3::CreateTranslation(0,speed * m_input->isMouseButtonDown(aie::INPUT_MOUSE_BUTTON_RIGHT)),Transform);
+    }
+
 }
 void Ship::Draw()
 {
     if(Sprite != nullptr)
     {
-        m_renderer2D->drawBox(m_hitBox->Position.x, m_hitBox->Position.y,m_hitBox->GetSize().x,m_hitBox->GetSize().y,Transform.GetRotationX(),1);
+        //m_renderer2D->drawBox(m_hitBox->Position.x, m_hitBox->Position.y,m_hitBox->GetSize().x,m_hitBox->GetSize().y,Transform.GetRotationX(),1);
         m_renderer2D->drawSprite(Sprite,m_pos.x,m_pos.y,m_size.x*2,m_size.y*2,Transform.GetRotationX(),1);
     }
     else
